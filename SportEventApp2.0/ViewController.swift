c0778fd3f1e6fcefeca4d98d91cf82e8b5e76e55
borys <ifedorchac@gmail.com>
2024-08-11 15:36:57 +0200

    Initial Commit

---
+++ SportEventApp2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportEventApp2.0
+//
+//  Created by borys on 11.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
